52387109c2b69ba89edb36b7525feb7015deac2f
Justin Hileman <justin@justinhileman.info>
2013-07-13 01:58:40 -0400

    Rewrite all Genghis JavaScript code to be AMD compatible

---
+++ assets/js/genghis/views/navbar.js
@@ -0,0 +1,19 @@
+define(['backbone', 'genghis/views', 'genghis/views/nav'], function(Backbone, Views, Nav) {
+
+    return Views.Navbar = Backbone.View.extend({
+        el: '.navbar',
+        events: {
+            'click a.brand': 'onClickBrand'
+        },
+
+        initialize: function() {
+            this.router  = this.options.router;
+            this.navView = new Nav({model: this.model, baseUrl: this.options.baseUrl});
+        },
+
+        onClickBrand: function(e) {
+            e.preventDefault();
+            this.router.navigate('', true);
+        }
+    });
+});
